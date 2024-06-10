7d72fe847b73f0108514c6870738bf97e597467e
gicrisf <giovanni.crisalfi@protonmail.com>
2024-06-11 00:33:08 +0200

    Cute macros

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,34 @@
 (defpackage clatrix
   (:use :cl))
+
 (in-package :clatrix)
 
-;; blah blah blah.
+(defclass matrix ()
+  ((rows :accessor matrix-rows
+         :initarg :rows
+         :initform (error "we need rows, dude"))
+   (A :accessor get-A
+      :initarg :A)
+   (b :accessor get-b
+      :initarg :b)))
+
+(defmethod initialize-instance :after ((matrix matrix) &key)
+  (with-accessors ((matrix-rows matrix-rows)) matrix
+    (setf (slot-value matrix 'A)
+          (mapcar (lambda (x) (car x)) matrix-rows))
+    (setf (slot-value matrix 'b)
+          (mapcar (lambda (x) (cadr x)) matrix-rows))))
+
+(defmacro make-matrix (rows)
+  `(progn (make-instance 'matrix :rows ,rows)))
+
+(defmacro with-matrix (rows &rest body)
+  `(let ((this-matrix (make-instance 'matrix :rows ,rows)))
+     (progn ,@body)))
+
+(defmacro letrix (bindings &rest body)
+  (let ((new-bindings
+          (mapcar (lambda (x) (list (car x)
+                          `(make-instance 'matrix :rows ,(cadr x))))
+                       bindings)))
+    `(let ,new-bindings ,@body)))
